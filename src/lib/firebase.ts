2f5832116ee84de50bb72c121a0209d04d3081a5
Duy Kha Nguyen <nguyenduykha12762@gmail.com>
2025-12-05 00:30:59 +0700

    update .env

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,14 +4,14 @@ import { getDatabase } from 'firebase/database';
 import { getAuth, signInAnonymously } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCqSnPxDuHd0xCjNYif267SLvQXH8Yunrg",
-  authDomain: "ndkha-webrtc.firebaseapp.com",
-  databaseURL: "https://ndkha-webrtc-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "ndkha-webrtc",
-  storageBucket: "ndkha-webrtc.firebasestorage.app",
-  messagingSenderId: "487730416478",
-  appId: "1:487730416478:web:febed5a727e29216c95dcf",
-  measurementId: "G-8E237DFCNC"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
